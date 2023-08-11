210d16447edbe048c4de20ac40c2086708cfbbf6
jacques <jacques.carlier@gmail.com>
2023-08-11 13:15:13 +0200

    renaming a page

--- src/components/routing/Router.jsx
+++ src/components/routing/Router.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import About from "../../pages/About";
 import Home from "../../pages/Home";
-import PageFetch from "../../pages/PageFetch"
+import PageFetch from "../../pages/HousingFile"
 import Page404 from "../../pages/Page404";
 
 function Router() {
